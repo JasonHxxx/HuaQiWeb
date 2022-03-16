7e34e3a361d55db23309645e95fef37c136f26ff
hy <191850059@smail.nju.edu.cn>
2022-03-17 00:33:12 +0800

    fix: merge错了

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -11,105 +11,28 @@ import PostHeader from "@/views/PostHeader";
 import EsgRe from "@/views/EsgRe";
 //stock
 import IndexStock from "@/views/stock/IndexStock";
+import GroupCard from "../views/stock/GroupCard";
 import StockChart from "@/views/stock/StockChart";
-import GroupCard from "@/views/stock/GroupCard";
-import StockLIst from "../views/stock/StockLIst";
 //test
 import ESGTestDo from "@/views/test/ESGTestDo";
 import RiskTestDo from "@/views/test/RiskTestDo";
 import TestMain from "@/views/test/TestMain";
+import StockLIst from "../views/stock/StockLIst";
 
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: DefaultLayout,
-    children: [
-      {
-        path: "/",
-        name: "home",
-        component: Home
-      },
-      {
-        path: "login",
-        name: "Login",
-        component: Login
-      },
-      {
-        path: "register",
-        name: "Register",
-        component: Register
-      }
-    ]
-  },
-  {
-    path: "/esg",
-    name: "esg",
-    component: DefaultLayout,
-    children: [
-      {
-        path: "intro",
-        name: "intro",
-        component: Esg
-      },
-      {
-        path: "rate",
-        name: "EsgRe",
-        component: EsgRe
-      }
-    ]
-  },
-  {
-    path: "/test",
-    name: "test",
-    component: DefaultLayout,
-    children: [
-      {
-        path: "/",
-        name: "test",
-        component: TestMain
-      },
-      {
-        path: "esgtest",
-        name: "ESGTest",
-        component: ESGTestDo
-      },
-      {
-        path: "risktest",
-        name: "RiskTest",
-        component: RiskTestDo
-      }
-    ]
-  },
-  {
-    path: "/stock",
-    name: "stock",
-    component: DefaultLayout,
-    children: [
-      {
-        path: "group",
-        name: "group",
-        component: GroupCard
-      },
-      {
-        path: "index",
-        name: "IndexStock",
-        component: IndexStock
-      },
-      {
-        path: "all",
-        name: "stockFrom",
-        component: StockLIst
-      },
-      {
-        path: "detail",
-        name: "stockChart",
-        component: StockChart
-      }
-    ]
+let router = new VueRouter({
+  mode:'history',//默认是hash模式
+  linkActiveClass:'menvscode-active',
+  scrollBehavior(to,from,savePosition){ // 在点击浏览器的“前进/后退”，或者切换导航的时候触发。
+    console.log(to) // to：要进入的目标路由对象，到哪里去
+    console.log(from) // from：离开的路由对象，哪里来
+    console.log(savePosition) // savePosition：会记录滚动条的坐标，点击前进/后退的时候记录值{x:?,y:?}
+    if(savePosition) {
+      return savePosition;
+    }else{
+      return {x:0,y:0}
+    }
   },
   routes:[
     {
@@ -121,11 +44,6 @@ const routes = [
           path: "/",
           name: "home",
           component: Home
-        },
-        {
-          path: "chart",
-          name: "Chart",
-          component: Chart
         },
         {
           path: "login",
@@ -184,11 +102,6 @@ const routes = [
       component: DefaultLayout,
       children: [
         {
-          path: "mygroup",
-          name: "mygroup",
-          component: myGroup
-        },
-        {
           path: "index",
           name: "IndexStock",
           component: IndexStock
@@ -202,6 +115,11 @@ const routes = [
           path: "detail",
           name: "stockChart",
           component: StockChart
+        },
+        {
+          path: "group",
+          name: "GroupCard",
+          component: GroupCard
         }
       ]
     },
@@ -213,6 +131,11 @@ const routes = [
   ]
 })
 
+// const router = new VueRouter({
+//   mode: "hash",
+//   base: process.env.BASE_URL,
+//   routes
+// });
 export default router
 
 // const router = new VueRouter({
@@ -234,6 +157,7 @@ export default router
 //   } else {
 //     next({ name: "Login" });
 //   }
-// });
+// })
+
 
 
