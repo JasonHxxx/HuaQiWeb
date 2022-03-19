baaa0f6f54a2b53a957f21676494bbba1d3ecbdf
hy <191850059@smail.nju.edu.cn>
2022-03-19 11:41:05 +0800

    feat: 组合推荐

---
+++ front_end/src/components/RecommendItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card style="width: 260px" class="mx-4">
+    <div class="right d-flex align-baseline ma-3">
+      <h3>推荐组合{{index+1}}</h3>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" text @click="addCombo">添加至自选</v-btn>
+    </div>
+    <div>
+      <v-card class="ma-2" elevation="1">
+        <v-list dense>
+          <v-list-item>
+            <v-list-item-content>Stock name</v-list-item-content>
+            <v-list-item-content class="align-end">
+              Stock rate
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item v-for="(item, i) in items" :key="i" class="my-2">
+            <v-list-item-content>{{item.name}}</v-list-item-content>
+            <v-list-item-content class="d-flex align-end justify-start">
+              <span style="color: red">+{{ item.data }}
+                <v-icon
+                color="red">
+                  mdi-arrow-up
+                </v-icon>
+              </span>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "RecommendItem",
+  props: {
+    index: {
+      type: Number,
+      default: 0
+    },
+    items: {
+      type: Array,
+      default: new Array([{}, {}, {}])
+    }
+  },
+  methods: {
+    addCombo(){
+      this.$emit("addCombo", [this.items[0].code,this.items[1].code,this.items[2].code],
+      [this.items[0].name, this.items[1].name, this.items[2].name])
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- front_end/src/views/stock/GroupCard.vue
+++ front_end/src/views/stock/GroupCard.vue
@@ -1,65 +1,83 @@
 <template>
-  <div>
-    <el-tabs
-      v-model="editableTabsValue"
-      type="card"
-      editable
-      style="margin-left: 10px; width: 95%"
-      @edit="handleTabsEdit"
-    >
-      <el-tab-pane
-        v-for="(item, index) in editableTabs"
-        :key="item.name"
-        :label="item.title"
-        :name="item.name"
+  <div class="mt-2">
+    <div class="ma-4"><v-chip color="primary" label>我的自选</v-chip></div>
+    <div>
+      <el-tabs
+        v-model="editableTabsValue"
+        type="card"
+        editable
+        style="margin-left: 16px; width: 95%"
+        @edit="handleTabsEdit"
       >
-        <span slot="label"
-          ><i class="el-icon-folder" />{{ " " + item.title }}</span
+        <el-tab-pane
+          v-for="(item, i) in editableTabs"
+          :key="item.name"
+          :label="item.title"
+          :name="item.name"
         >
-        <el-table
-          border
-          height="300"
-          :data="item.comboData"
-          class="table"
-          stripe
-        >
-          <el-table-column prop="code" label="Stock code" />
-          <el-table-column prop="name" label="Stock name" />
-          <el-table-column prop="price" label="Stock price" />
-          <el-table-column label="action" align="center">
-            <template slot="header">
-              <el-input
-                v-model="stock_code"
-                style="width: 150px; margin-right: 10px"
-                size="mini"
-                placeholder="input stock code"
-                type="number"
-                @blur="inputCheck()"
-              ></el-input>
-              <el-button
-                class="el-icon-plus"
-                type="primary"
-                size="mini"
-                @click.prevent="addStock(index)"
-              />
-            </template>
-            <template slot-scope="scope">
-              <i
-                class="el-icon-delete"
-                style="font-size:18px;cursor: pointer;"
-                @click="handleDelete(scope.$index, scope.row)"
-              />
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-tab-pane>
-    </el-tabs>
+          <span slot="label"
+            ><i class="el-icon-folder" />{{ " " + item.title }}</span
+          >
+          <div class="justify-end d-flex">
+            <el-input
+              v-model="stock_code"
+              style="width: 150px; margin-right: 10px"
+              size="mini"
+              right
+              placeholder="input stock code"
+              type="number"
+              @blur="inputCheck()"
+            ></el-input>
+            <el-button
+              class="el-icon-plus"
+              type="primary"
+              size="mini"
+              @click.prevent="addStock(i)"
+            />
+          </div>
+          <el-table
+            border
+            height="280"
+            :data="item.comboData"
+            class="table"
+            stripe
+          >
+            <el-table-column prop="code" label="Stock code" />
+            <el-table-column prop="name" label="Stock name" />
+            <el-table-column prop="price" label="Stock price" />
+            <el-table-column label="action" align="center">
+              <template slot-scope="scope">
+                <i
+                  class="el-icon-delete"
+                  style="font-size:18px;cursor: pointer;"
+                  @click="handleDelete(scope.$index, i)"
+                />
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <div class="ma-4"><v-chip color="primary" label>推荐组合</v-chip></div>
+    <div class="d-flex justify-space-between" style="width: 70%;">
+      <recommend-item
+        v-for="(re, i) in recommendData"
+        :key="i"
+        :index="i"
+        :items="re"
+        @addCombo="addCombo"
+      ></recommend-item>
+    </div>
   </div>
 </template>
 
 <script>
+import RecommendItem from "@/components/RecommendItem";
 export default {
   name: "GroupCard",
+  components: {
+    RecommendItem
+  },
   data() {
     return {
       stock_code: "",
@@ -73,7 +91,24 @@ export default {
           comboData: []
         }
       ],
-      tabIndex: 1
+      tabIndex: 1,
+      recommendData: [
+        [
+          { name: "hhh", data: "1.53%", code: "123411" },
+          { name: "hhh", data: "1.23%", code: "12346" },
+          { name: "hhh", data: "2s.7%", code: "12335" }
+        ],
+        [
+          { name: "hhh", data: "1.53%", code: "123411" },
+          { name: "hhh", data: "1.23%", code: "12346" },
+          { name: "hhh", data: "2s.7%", code: "12335" }
+        ],
+        [
+          { name: "h", data: "1.53%", code: "123411" },
+          { name: "hhh", data: "1.23%", code: "12346" },
+          { name: "hhhh", data: "2s.7%", code: "12335" }
+        ],
+      ]
     };
   },
   mounted() {
@@ -98,9 +133,27 @@ export default {
     console.log(this.editableTabs);
   },
   methods: {
-    change(e) {
-      console.log(e);
-      this.$forceUpdate();  //强制刷新
+    addCombo(codes,names) {
+      const newTabIndex = ++this.tabIndex + "";
+      this.editableTabs.push({
+        title: "Combo" + ++this.addIndex,
+        name: newTabIndex,
+        comboData: []
+      });
+      this.editableTabsValue = newTabIndex;
+      this.currentIndex = newTabIndex;
+      console.log(newTabIndex+"new");
+      console.log(this.addIndex+"add");
+      let tabIndex = this.addIndex-1;
+      for (let i = 0; i < 3; i++) {
+        const stockRow = {
+          code: codes[i],
+          name: names[i],
+          price: "" + 100 + Math.round(Math.random() * 1000)
+        };
+        this.editableTabs[tabIndex].comboData.push(stockRow);
+        this.stock_code = "";
+      }
     },
     inputCheck() {
       if (this.stock_code.length !== 6) {
@@ -125,15 +178,14 @@ export default {
       this.editableTabs[index].comboData.push(stockRow);
       this.stock_code = "";
     },
-    handleDelete(index, row) {
-      console.log(row);
+    handleDelete(index, i) {
       this.$confirm("sure to delete this stock?", "tips", {
         confirmButtonText: "confirm",
         cancelButtonText: "cancel",
         type: "warning"
       })
         .then(() => {
-          this.editableTabs[index].comboData.splice(index, 1);
+          this.editableTabs[i].comboData.remove(index);
           this.$message({
             type: "success",
             message: "delete successfully!"
@@ -148,13 +200,6 @@ export default {
     },
     handleTabsEdit(targetName, action) {
       if (action === "add") {
-        // if (this.tabIndex > 9) {
-        //   this.$message({
-        //     type: 'warning',
-        //     message: 'too many combos !'
-        //   })
-        //   return
-        // }
         const newTabIndex = ++this.tabIndex + "";
         this.editableTabs.push({
           title: "Combo" + ++this.addIndex,
@@ -174,7 +219,6 @@ export default {
         let activeName = self.editableTabsValue;
         console.log("a:" + activeName);
         if (activeName === targetName) {
-          console.log("aaa");
           tabs.forEach((tab, index) => {
             if (tab.name === targetName) {
               const nextTab = tabs[index + 1] || tabs[index - 1];
